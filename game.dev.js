599ba67ccd0e3a09e1568fb6b99d57916da2cd86
Basit Oketunde <basitoketunde@Basits-MacBook-Pro.local>
2021-11-17 14:52:05 +0000

    added event listeners to buttons

---
+++ game.dev.js
@@ -0,0 +1,18 @@
+"use strict";
+
+//1. Think about what you are trying to achieve - RPS
+//2. Think about what the game has to do 
+//         - select R, P or S
+//         - CPU generate dandom R P or S as soon as user chooses
+//        - display score on the game
+//        - determine who wins by using condiational statements for R P S
+//         - store the scores and calculate who wins
+// do one thing at a time and test each stage
+//
+var selectChoice = document.querySelectorAll(".selection");
+console.log(selectChoice);
+selectChoice.forEach(function (choice) {
+  choice.addEventListener("click", function () {
+    alert("the button was clicked");
+  });
+});
---
+++ game.js
@@ -0,0 +1,19 @@
+//1. Think about what you are trying to achieve - RPS
+//2. Think about what the game has to do 
+//         - select R, P or S
+//         - CPU generate dandom R P or S as soon as user chooses
+ //        - display score on the game
+ //        - determine who wins by using condiational statements for R P S
+//         - store the scores and calculate who wins
+            
+// do one thing at a time and test each stage
+//
+ const selectChoice = document.querySelectorAll(".selection")
+ console.log(selectChoice);
+
+ selectChoice.forEach(choice => { 
+     choice.addEventListener("click", () => {
+         alert("the button was clicked")
+     })
+     
+ });
